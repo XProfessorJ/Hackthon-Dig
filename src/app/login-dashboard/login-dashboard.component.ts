298abbd22db0a9fe3ecc60968c51d77a6d01ac94
Jimmy <15021734680@163.com>
2020-09-15 18:31:10 +0800

    init

---
+++ src/app/login-dashboard/login-dashboard.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login-dashboard',
+  templateUrl: './login-dashboard.component.html',
+  styleUrls: ['./login-dashboard.component.scss'],
+})
+export class LoginDashboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
